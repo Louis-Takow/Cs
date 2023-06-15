6ec4d8866a78121d655c31e30b3e95cc32868186
unknown <louistt1995@gmail.com>
2023-06-15 09:50:07 +0100

    bol

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -7,7 +7,8 @@ class program
     static void Main(string[] args)
     {
         var character = 'A';
-        int number = 5
+        int number = 5;
+        var isWorking = false;
         System.Console.WriteLine(character);
         System.Console.WriteLine(number);
     }
