5a26f0f19a3dafdbe57307a942dcbfe504d31096
unknown <louistt1995@gmail.com>
2023-06-15 09:53:44 +0100

    new3

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -7,7 +7,7 @@ class program
     static void Main(string[] args)
     {
         var character = 'A';
-        int number = 5;
+        
         
         
         System.Console.WriteLine(character);
