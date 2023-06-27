8679a7d922ae9139a52be1cb28426f49fdc044f7
unknown <louistt1995@gmail.com>
2023-06-27 11:27:05 +0100

    int

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -12,7 +12,6 @@ class program
         string word = "believe";
         bool isForming = true;
         double decimal = 4.5d;
-        float floating = 7.9f;
         System.Console.WriteLine(character);
     }
 }
