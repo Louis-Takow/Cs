b04432854b0ec65ea5f66b254d477a882648aacc
unknown <louistt1995@gmail.com>
2023-07-04 10:30:56 +0100

    int

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -7,9 +7,6 @@ class program
     static void Main(string[] args)
     {
         var character = 'A';
-        bool isForming = false;
-        int number = 0;
-        string word = "believe";
         System.Console.WriteLine(character);
     }
 }
