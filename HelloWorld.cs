cd908231d240c9bf7aebf34fa85b5301b96a4f02
unknown <louistt1995@gmail.com>
2023-07-09 19:22:47 +0100

    int

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -9,6 +9,10 @@ class program
         var character = 'A';
         bool isForming = false;
         int number = 0;
+        string word = "believe";
+        bool isForming = true;
+        double decimal = 4.5d;
+        float floating = 7.9f;
         System.Console.WriteLine(character);
     }
 }
