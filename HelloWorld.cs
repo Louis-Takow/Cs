95122fb52b995bb41a8a480492c85b9869184d57
unknown <louistt1995@gmail.com>
2023-08-26 10:58:35 +0100

    2nd commit

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -9,6 +9,8 @@ class program
         var character = 'A';
         bool isForming = false;
         int number = 0;
+        string word = "believe";
+        bool isForming = true;
         System.Console.WriteLine(character);
     }
 }
